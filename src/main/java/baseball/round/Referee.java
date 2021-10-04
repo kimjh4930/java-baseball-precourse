ee15a7e1b02b178bc8884948734584f421adeba5
Junha Kim <kimjh4930@gmail.com>
2021-10-04 18:36:20 +0900

    feat(Referee) : Referee 의 역할을 정의한다.
    - getPitchersBall : Pitcher 에게 공을 받는다.
    - judge : Hitter에게 공을 받아 Pitcher와 비교해 결과를 판정한다.
    - strike : rule에서 정의한 스트라이크에 부합하는 숫자의 개수를 반환.
    - ball : rule에서 정의한 볼에 부합하는 숫자의 개수를 반환
    - gameSet : 게임종료 여부 판

---
+++ src/main/java/baseball/round/Referee.java
@@ -0,0 +1,45 @@
+package baseball.round;
+
+public class Referee {
+    private final Rule rule;
+
+    private ResultBoard board;
+    private NumbersBall pitchersBall;
+    private NumbersBall hittersBall;
+
+    public Referee (Rule rule){
+        this.rule = rule;
+        board = new ResultBoard();
+    }
+
+    public void pitchersBall (NumbersBall pitchersBall){
+        this.pitchersBall = pitchersBall;
+    }
+
+    public boolean judge (NumbersBall hittersBall){
+        this.hittersBall = hittersBall;
+
+        int strike = strike();
+        int ball = ball();
+
+        call(strike, ball);
+
+        return gameSet(strike);
+    }
+
+    private void call (int strike, int ball){
+        System.out.println(board.read(strike, ball));
+    }
+
+    private int strike (){
+        return rule.strike(pitchersBall.toList(), hittersBall.toList());
+    }
+
+    private int ball (){
+        return rule.ball(pitchersBall.toList(), hittersBall.toList());
+    }
+
+    private boolean gameSet (int strike){
+        return strike == rule.getDigit();
+    }
+}
---
+++ src/test/java/baseball/round/RefereeTest.java
@@ -0,0 +1,89 @@
+package baseball.round;
+
+import baseball.rule.ThreeNumbersRule;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RefereeTest {
+    private PrintStream standardOut;
+    private OutputStream captor;
+
+    private Referee referee;
+
+    @BeforeEach
+    protected void setUp (){
+        standardOut = System.out;
+        captor = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(captor));
+
+        referee = RefereeConfig.referee();
+    }
+
+    @DisplayName("결과를 판정한다.")
+    @ParameterizedTest
+    @CsvSource(value = {
+                "123:124:2스트라이크:false",
+                "135:246:낫싱:false",
+                "123:312:3볼:false",
+                "456:456:3스트라이크:true",
+                "123:321:1스트라이크 2볼:false"},
+                delimiter = ':')
+    void judge (String pitcher, String hitter, String result, boolean gameSet){
+        //given
+        NumbersBall pitchers = new NumbersBall(stringAsList(pitcher));
+        NumbersBall hitters = new NumbersBall(stringAsList(hitter));
+
+        //when
+        referee.pitchersBall(pitchers);
+
+        //then
+        assertThat(referee.judge(hitters)).isEqualTo(gameSet);
+        verity(result);
+    }
+
+    @AfterEach
+    void tearDown (){
+        System.setOut(standardOut);
+        System.out.println(output());
+    }
+
+    private List<Integer> stringAsList (String input){
+        List<Integer> numbers = new ArrayList<>();
+
+        for(char number : input.toCharArray()){
+            numbers.add(number - '0');
+        }
+
+        return numbers;
+    }
+
+    private void verity (final String... args){
+        assertThat(output()).contains(args);
+    }
+
+    private String output(){
+        return captor.toString().trim();
+    }
+
+    static class RefereeConfig {
+        static Referee referee (){
+            return new Referee(rule());
+        }
+
+        static Rule rule (){
+            return new ThreeNumbersRule();
+        }
+    }
+
+}
